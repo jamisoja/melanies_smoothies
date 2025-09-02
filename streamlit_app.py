415a0a6e914d96ce6c75110149dc5480e1afd29a
jamisoja <jamisoja@ph.ibm.com>
2025-09-02 17:48:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
   """
 )
 
-name_on_order = st.text_input('Nameon Smoothie: ')
+name_on_order = st.text_input('Name on Smoothie: ')
 st.write('The name on your Smoothie will be: ',name_on_order)
 
 cnx = st.connection("snowflake")
@@ -40,7 +40,7 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
